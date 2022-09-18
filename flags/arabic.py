5f7cf209702ba3b2b377985b719eb905cee0270b
itachisalah <95444298+itachisalah@users.noreply.github.com>
2022-09-18 20:00:11 +0300

    main

---
+++ flags/arabic.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from arabic_reshaper import arabic_reshaper
+from bidi.algorithm import get_display
+
+
+class MyApp(App):
+    def build(self):
+        pass
+
+
+MyApp().App()
+
